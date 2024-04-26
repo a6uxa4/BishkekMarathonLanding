9732326e0085242f631e3370d5af2999b92b6b00
Abu <104144116+a6uxa4@users.noreply.github.com>
2024-04-26 15:33:42 +0600

    fix

--- src/layout/Header.tsx
+++ src/layout/Header.tsx
@@ -14,7 +14,7 @@ export const Header = () => {
     <header className="w-full fixed bg-customBlack z-50">
       <div className="w-full h-[67px] container py-4 mx-auto flex justify-between items-center">
         <div className="flex items-center justify-between">
-          <Image src="/icon/logo.svg" alt="logo" width={128} height={26} />
+          <Image src="/icon/Logo.svg" alt="logo" width={128} height={26} />
         </div>
         <div className="relative inset-x-0 z-20 w-auto h-[67px] px-6 py-4 flex items-center justify-end">
           <div className="flex flex-row mx-6">
@@ -42,7 +42,10 @@ export const Header = () => {
           </button>
         </div>
       </div>
-      <motion.div className="fixed left-0 right-0 h-1 bg-customGreen top-[67px]" style={{ scaleX }} />
+      <motion.div
+        className="fixed left-0 right-0 h-1 bg-customGreen top-[67px]"
+        style={{ scaleX }}
+      />
     </header>
   );
 };

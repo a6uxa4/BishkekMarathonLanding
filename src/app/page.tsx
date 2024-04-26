0db924d3a30e2023e63b8945cba3cf9c14cbad82
Abu <104144116+a6uxa4@users.noreply.github.com>
2024-04-26 12:38:48 +0600

    fix scroll

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,14 @@
+"use client";
+
 import clsx from "clsx";
+import { useMotionValueEvent, useScroll, useSpring } from "framer-motion";
+import { useEffect, useState } from "react";
 
 function Image({ id }: { id: number }) {
   return (
     <section
       className={clsx(
-        "w-[100vw] h-[100vh] flex justify-center items-center relative snap-center"
+        "w-[100vw] h-[100vh] flex justify-center items-center relative !snap-center"
       )}
     >
       <div>HELLO {id}</div>
@@ -12,11 +16,34 @@ function Image({ id }: { id: number }) {
   );
 }
 export default function Home() {
+  const { scrollY } = useScroll();
+  const [scrollValue, setScrollValue] = useState<number>(1);
+
+  useEffect(() => {
+    const updateScrollValue = () => setScrollValue(scrollY.get());
+    const unsubscribe = scrollY.onChange(updateScrollValue);
+    return () => unsubscribe();
+  }, [scrollY]);
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24 bg-white">
+    <main
+      className={clsx(
+        "flex min-h-screen flex-col items-center justify-between p-24 bg-white",
+        {
+          "overflow-auto": scrollValue >= 3244,
+        }
+      )}
+    >
       {[1, 2, 3, 4, 5].map((image) => (
         <Image id={image} key={image} />
       ))}
+      <section
+        className={clsx(
+          "w-[100vw] h-[100vh] flex justify-center items-center relative"
+        )}
+      >
+        <div>HELLO 6</div>
+      </section>
     </main>
   );
 }

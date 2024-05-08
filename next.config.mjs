a4d7b51b3ee6452482b0f5f611ab39780f726638
Abu <104144116+a6uxa4@users.noreply.github.com>
2024-05-08 18:12:40 +0600

    finish project

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,13 @@ import createNextIntlPlugin from "next-intl/plugin";
 const withNextIntl = createNextIntlPlugin();
 
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  experimental: {
+    optimizePackageImports: [
+      "@nextui-org/react",
+      "react-intersection-observer",
+    ],
+  },
+};
 
 export default withNextIntl(nextConfig);
